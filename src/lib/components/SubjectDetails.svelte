2ff3ff8e600c97b30d51dc7282caee89c66070c6
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-03 01:57:31 -0400

    make it actually functional

--- src/lib/components/SubjectDetails.svelte
+++ src/lib/components/SubjectDetails.svelte
@@ -3,8 +3,7 @@
 -->
 
 <script lang="ts">
-	import { invalidate } from '$app/navigation';
-import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } from '$lib/types';
+	import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } from '$lib/types';
 	import { ClassGroupChatMembership } from '$lib/types';
 	import { getContext } from 'svelte';
 	import type { Readable } from 'svelte/store';
@@ -21,20 +20,46 @@ import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } fr
 		return json;
 	}
 
+	$: subjectDetailsPromise = getSubjectDetails(subject.number);
+
+	// It is important to use the canonical class number to check the membership
+	// (NOTE: we are trusting the client, but I think that is fine)
+	// TODO: use the canonical number on chat creation (necessary so it is fine)
+
 	async function getSubjectChat(number: string): Promise<SubjectChatDetails> {
-		const response = await fetch(`/classes/api/chat/${number}`);
+		const details = await subjectDetailsPromise;
+		const { canonicalNumber } = details;
+		const response = await fetch(`/classes/api/chat/${canonicalNumber}`);
 		const json = await response.json();
 		return json;
 	}
 
 	async function getMembership(number: string, mxid: string): Promise<ClassGroupChatMembership> {
-		const response = await fetch(`/classes/api/chat/${number}/member/${mxid}`);
+		const details = await subjectDetailsPromise;
+		const { canonicalNumber } = details;
+		const response = await fetch(`/classes/api/chat/${canonicalNumber}/member/${mxid}`);
 		const json: MembershipResult = await response.json();
 		return json.membership;
 	}
+
+	let loading: boolean = false;
+
+	async function invite(number: string, mxid: string): Promise<void> {
+		loading = true;
+		const response = await fetch(`/classes/api/chat/${number}/member/${mxid}`, {
+			method: 'PUT'
+		});
+		// TODO: check if successful otherwise show an error
+		subject = subject; // to reload upon inviting (hack?)
+		loading = false;
+	}
 </script>
 
-{#await Promise.all([getSubjectDetails(subject.number), getSubjectChat(subject.number), getMembership(subject.number, $mxid)])}
+<!-- TODO: is promise.all actually what we want?
+		   maybe we want to show subject info while the membership is loading, 
+		   especially if we just clicked the join button
+-->
+{#await Promise.all([getSubjectDetails(subject.number), getSubjectChat(subject.number), getMembership(subject.number, $mxid)] )}
 	<div class="section">
 		<div class="container is-max-desktop">
 			<h1 class="title">{subject.number}</h1>
@@ -56,11 +81,16 @@ import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } fr
 				</div>
 				<div class="level-right">
 					<div class="level-item">
-						<!-- TODO: is-loading if loading, disabled if already inside -->
-						<button class="button is-link" disabled={membership !== Membership.not_joined}>
+						<button
+							class:is-loading={loading}
+							class="button is-link"
+							disabled={loading || membership !== Membership.not_joined}
+							on:click={() => invite(canonicalSubject.canonicalNumber, $mxid)}
+						>
 							<div class="icon-text">
 								<span class="icon">
-									<i class="fa-solid"
+									<i
+										class="fa-solid"
 										class:fa-user-plus={membership === Membership.not_joined}
 										class:fa-user-clock={membership === Membership.invited}
 										class:fa-user-check={membership === Membership.joined}
@@ -81,6 +111,7 @@ import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } fr
 							</div>
 						</button>
 						<span class="button is-static">
+							<!-- substract 1 since the bot doesn't count -->
 							<span>{chat.numMembers}</span>
 							<span class="icon">
 								<i class="fa-solid fa-users" />
@@ -92,12 +123,16 @@ import type { Subject, SubjectDetails, MembershipResult, SubjectChatDetails } fr
 			{#if membership == Membership.invited}
 				<div class="notification is-warning">
 					<!--button class="delete" /-->
-					<p>You have already been invited to the chat. To accept the invite, go to
-					<a href="https://matrix.mit.edu">
-						matrix.mit.edu<span class="icon"><i class="fa fa-external-link-alt" /></span>
-					</a>.</p>
-					<p>You can also download <a href="https://matrix.mit.edu/mobile_guide/">Element</a> on
-					your phone, and change your server to <strong>matrix.mit.edu</strong> before logging in.</p>
+					<p>
+						You have already been invited to the chat. To accept the invite, go to
+						<a href="https://matrix.mit.edu">
+							matrix.mit.edu<span class="icon"><i class="fa fa-external-link-alt" /></span>
+						</a>.
+					</p>
+					<p>
+						You can also download <a href="https://matrix.mit.edu/mobile_guide/">Element</a> on your
+						phone, and change your server to <strong>matrix.mit.edu</strong> before logging in.
+					</p>
 				</div>
 			{/if}
 			<p>{canonicalSubject.description}</p>
