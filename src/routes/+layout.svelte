e2638ce54204c7ac86c51957a6bf1efdd80fdfdc
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-03 00:31:23 -0400

    footer stub

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
 	import 'bulma/css/bulma.css';
-    import '@fortawesome/fontawesome-free/css/all.css'
+	import '@fortawesome/fontawesome-free/css/all.css';
 </script>
 
-<slot/>
+<slot />
+
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			Brought to you by <a href="https://sipb.mit.edu" target="_blank" rel="noopener">SIPB</a>.
+            The source code is available on <a href="https://github.com/gabrc52/class_group_chats/">GitHub</a>.
+		</p>
+	</div>
+</footer>
