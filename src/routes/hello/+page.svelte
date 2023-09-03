40a44977c3d0a921fd8ab6c06a4d5b0a9327d82e
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-03 02:11:43 -0400

    remove demo pages

--- src/routes/hello/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let data;
-</script>
-
-<div class="box">
-    <h1>Hello world! I mean, Matrix.</h1>
-    <p>I (the server) am authenticated as {data.user}.</p>
-</div>
--- src/routes/hello2/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let data;
-</script>
-
-<h1>Hello world! I mean, Matrix.</h1>
-<p>I (the server) am authenticated as {data.user}.</p>
