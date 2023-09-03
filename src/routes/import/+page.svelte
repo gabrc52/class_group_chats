cdcbf1d90dcddbce862438a7b8db4665d958e389
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-03 02:27:21 -0400

    add stub import page

---
+++ src/routes/import/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import { page } from '$app/stores';
+
+	let importingFrom: string;
+	let classNumbers: string[] = [];
+
+	$: {
+		importingFrom = $page.url.searchParams.get('via') ?? 'unknown source';
+		classNumbers = $page.url.searchParams.getAll('class');
+	}
+</script>
+
+<Header />
+
+<!-- TODO: maybe center?-->
+<div class="section content">
+	<div class="container is-max-desktop">
+		<h1 class="title">Importing from {importingFrom}</h1>
+		<p class="subtitle">TODO(rgabriel): Implement</p>
+        <p class="subtitle">Join some of them or join all of them, your pick.</p>
+        <!-- TODO: make UI actually functional, using similar endpoints to SubjectDetails.svelte -->
+        <ul>
+            {#each classNumbers as classNumber}
+                <li>{classNumber}</li>
+            {/each}
+        </ul>
+	</div>
+</div>
---
+++ src/routes/import/README.md
@@ -0,0 +1,7 @@
+### How to call?
+
+Redirect the user to a URL like the following:
+
+`https://matrix.mit.edu/classes/import?via=Hydrant&class=6.1010&class=18.02&class=8.01`
+
+Replace `Hydrant` with your source platform.
