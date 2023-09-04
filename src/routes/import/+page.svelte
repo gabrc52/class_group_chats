876d32f0ffdd02e561eab54af641b2782a7071a4
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-03 23:23:30 -0400

    stub import page

--- src/routes/import/+page.svelte
+++ src/routes/import/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
 	import { page } from '$app/stores';
+	import SubjectDetails from '$lib/components/SubjectDetails.svelte';
 
 	let importingFrom: string;
 	let classNumbers: string[] = [];
@@ -11,8 +11,6 @@
 	}
 </script>
 
-<Header />
-
 <!-- TODO: maybe center?-->
 <div class="section content">
 	<div class="container is-max-desktop">
@@ -20,10 +18,20 @@
 		<p class="subtitle">TODO(rgabriel): Implement</p>
         <p class="subtitle">Join some of them or join all of them, your pick.</p>
         <!-- TODO: make UI actually functional, using similar endpoints to SubjectDetails.svelte -->
-        <ul>
-            {#each classNumbers as classNumber}
-                <li>{classNumber}</li>
-            {/each}
-        </ul>
+
+		<!-- TODO: use a smaller component - we are reusing the existing one for demo purposes
+			
+			it would be good to turn SubjectDetails into SubjectContainer and just pass the necessary
+			stuff as props. or some other way to keep it DRY
+		-->
 	</div>
+	{#each classNumbers as classNumber}
+		<SubjectDetails subject={{
+			number: classNumber,
+			// leave these unfilled for now, not that we're actually using it like this
+			name: '',
+			level: '',
+			oldNumber: undefined,
+		}}/>
+	{/each}
 </div>
