23527fef0679530f3f5bcfdaf286e8ba151fa884
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-04 04:02:29 -0400

    extract kerb input to own component

---
+++ src/lib/components/KerbInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { PUBLIC_MATRIX_HOMESERVER } from "$env/static/public";
+	import { getContext } from "svelte";
+	import type { Readable, Writable } from "svelte/store";
+
+    export let username: Writable<string>;
+    const mxid: Readable<string> = getContext('mxid');
+</script>
+
+<div class="section" style="padding-bottom: 0;">
+	<div class="container is-max-desktop">
+        <label for="kerb" class="label">Enter your kerb</label>
+		<div class="field is-horizontal has-addons">
+			<p class="control">
+				<span class="button is-static">@</span>
+			</p>
+            <p class="control">
+				<input class="input" type="text" id="kerb" name="kerb" placeholder="jflorey" bind:value={$username} />
+			</p>
+			<p class="control">
+				<span class="button is-static">:{PUBLIC_MATRIX_HOMESERVER}</span>
+			</p>
+		</div>
+        {#if $username}
+            <p class="help">The full thing ({$mxid}) is your global Matrix username.</p>
+        {/if}
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 	import { setContext } from 'svelte';
 	import { persisted } from 'svelte-local-storage-store';
     import { derived, readonly } from 'svelte/store';
+	import KerbInput from '$lib/components/KerbInput.svelte';
 
 	// TODO: use some actual authentication mechanism
     const username = persisted<string>('username', '');
@@ -21,25 +22,7 @@
 -->
 <Header />
 
-<div class="section" style="padding-bottom: 0;">
-	<div class="container is-max-desktop">
-        <label for="kerb" class="label">Enter your kerb</label>
-		<div class="field is-horizontal has-addons">
-			<p class="control">
-				<span class="button is-static">@</span>
-			</p>
-            <p class="control">
-				<input class="input" type="text" id="kerb" name="kerb" placeholder="jflorey" bind:value={$username} />
-			</p>
-			<p class="control">
-				<span class="button is-static">:{PUBLIC_MATRIX_HOMESERVER}</span>
-			</p>
-		</div>
-        {#if $username}
-            <p class="help">The full thing ({$mxid}) is your global Matrix username.</p>
-        {/if}
-	</div>    
-</div>
+<KerbInput {username}/>
 
 {#if $username}
 	<slot />
