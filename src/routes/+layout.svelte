63543e9b08cb7ba7f0e9a124936a24f8506c26a1
Gabriel Rodríguez <rgabriel@mit.edu>
2024-02-04 15:44:09 -0500

    add skeleton.dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,51 @@
 <script lang="ts">
-	import 'bulma/css/bulma.css';
-	import '@fortawesome/fontawesome-free/css/all.css';
-
-	import { PUBLIC_MATRIX_HOMESERVER } from '$env/static/public';
-	import Header from '$lib/components/Header.svelte';
-	import { setContext } from 'svelte';
-	import { persisted } from 'svelte-local-storage-store';
-    import { derived, readonly } from 'svelte/store';
-	import KerbInput from '$lib/components/KerbInput.svelte';
-
-	// TODO: use some actual authentication mechanism
-    const username = persisted<string>('edu.mit.sipb.subjects.username', '');
-    setContext('username', username);
-	const mxid = derived(username, (username) => `@${username.trim().toLowerCase()}:${PUBLIC_MATRIX_HOMESERVER}`);
-    setContext('mxid', mxid);
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import FeedbackIcon from "svelte-material-icons/MessageAlertOutline.svelte";
+	import GitHubIcon from "svelte-material-icons/Github.svelte";
 </script>
 
-<!--
-	if we end up wanting to customize the header we can use named slots
-	but I think it's good to reuse the "authentication"
--->
-<Header />
-
-<slot />
-
-<footer class="footer">
-	<div class="content has-text-centered">
-		<p>
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Class group chats via [matrix]</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/sipb/class_group_chats"
+					target="_blank"
+					rel="noreferrer"
+				>
+				<div style="padding-right: 4px;"><GitHubIcon/></div>
+					Source code
+				</a>
+				<a
+					id="feedback-button"
+					class="btn btn-sm variant-ghost-surface"
+					href="https://docs.google.com/forms/d/e/1FAIpQLSfIbAmtZeqLWCRx3L8_Hj28Nwh6TtsCjxN7OZjl8UisDkHgHw/viewform"
+					target="_blank"
+					rel="noreferrer"
+				>
+				<div style="padding-right: 4px;"><FeedbackIcon/></div>
+					Give us feedback!
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="pageFooter">
+		<!-- TODO: footer styling -->
+		<!-- <p>
 			Brought to you by <a href="https://sipb.mit.edu" target="_blank" rel="noopener">SIPB</a>.
             The source code is available on <a href="https://github.com/gabrc52/class_group_chats/">GitHub</a>.
 		</p>
 		<p>
 			Contact: <a href="mailto:matrix@mit.edu">matrix@mit.edu</a>
-		</p>
-	</div>
-</footer>
+		</p> -->
+	</svelte:fragment>
+	<slot />
+</AppShell>
+
