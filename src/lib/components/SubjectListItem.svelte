4a03ed4dafcc14ceea9f9e44edbf2efe5b52e3fa
Gabriel Rodríguez <rgabriel@mit.edu>
2024-02-06 23:56:52 -0500

    also known as

--- src/lib/components/SubjectListItem.svelte
+++ src/lib/components/SubjectListItem.svelte
@@ -4,31 +4,44 @@
 -->
 
 <script lang="ts">
-	import { onMount } from "svelte";
-    import type { SubjectDetailsOracle } from "$lib/types";
-	import type { MouseEventHandler } from "svelte/elements";
-	import { slide } from "svelte/transition";
+	import { onMount } from 'svelte';
+	import type { SubjectDetailsOracle } from '$lib/types';
+	import type { MouseEventHandler } from 'svelte/elements';
+	import { slide } from 'svelte/transition';
 
 	export let subject: string;
-    let subjectDetails: SubjectDetailsOracle | undefined;
+	let subjectDetails: SubjectDetailsOracle | undefined;
 	export let onDelete: MouseEventHandler<HTMLButtonElement>;
 
-    onMount(async () => {
-        const response = await fetch(`/classes/api/subjectWarehouse/${subject}`);
-        const json = await response.json();
-        subject = json.masterNumber;
-        subjectDetails = json;
-    });
+	onMount(async () => {
+		const response = await fetch(`/classes/api/subjectWarehouse/${subject}`);
+		const json = await response.json();
+		subject = json.masterNumber;
+		subjectDetails = json;
+	});
 </script>
 
 <div transition:slide class="hover:bg-secondary-500 hover:bg-opacity-35">
 	<!-- so the material icon is too small - i tried copying just the svg and it's too big -->
-	<button on:click={onDelete} type="button" class="btn-icon btn-icon-sm variant-filled"><i class="fa-solid fa-trash"></i></button>
-    <span class="flex-auto space-x-2">
-        <span class="font-bold">{subject}</span>
-        <!-- TODO: it would be good to use the longer name -->
-        {#if subjectDetails}
-            <span class="opacity-70">{subjectDetails?.title}</span>
-        {/if}
-    </span>
+	<button on:click={onDelete} type="button" class="btn-icon btn-icon-sm variant-filled"
+		><i class="fa-solid fa-trash"></i></button
+	>
+	<span class="flex-auto">
+		<dt>
+			<span class="font-bold">{subject}</span>
+			<!-- TODO: it would be good to use the longer name -->
+			{#if subjectDetails}
+				<span class="opacity-70">{subjectDetails?.title}</span>
+			{/if}
+		</dt>
+		{#if subjectDetails && subjectDetails.otherNumbers.length > 0}
+			<dd class="pl-0">
+				<span class="opacity-50 text-sm"
+					>Also known as: {subjectDetails.otherNumbers
+						.filter((s) => s !== subject)
+						.join(', ')}</span
+				>
+			</dd>
+		{/if}
+	</span>
 </div>
