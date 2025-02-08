cdf167ac60841fced7e795e506e2a83d3466c4c5
Gabriel Rodríguez <rgabriel@mit.edu>
2025-02-08 17:47:20 -0500

    allow middle click

--- src/lib/components/MatrixJoin.svelte
+++ src/lib/components/MatrixJoin.svelte
@@ -69,7 +69,11 @@
 		{/if}
 		To view your class group chats, you should open <a href={PUBLIC_ELEMENT_LINK} target="element" on:click={() => $hasOpenedElement = true}> <ElementLogo/> matrix.mit.edu</a>. Click the button below to do so:
 	</p>
-	<a type="button" class="btn variant-filled-success mx-auto" href={PUBLIC_ELEMENT_LINK} target="element" on:click={() => $hasOpenedElement = true}>
+	<a type="button" class="btn variant-filled-success mx-auto" href={PUBLIC_ELEMENT_LINK} target="element" on:click={() => $hasOpenedElement = true} on:auxclick={(e) => {
+		if (e.button == 1) {
+			$hasOpenedElement = true;
+		}
+	}}>
 		<span><i class="fa-solid fa-arrow-up-right-from-square"></i></span>
 		<span>Open matrix.mit.edu now</span>
 	</a>
